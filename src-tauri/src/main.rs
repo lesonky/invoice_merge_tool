256b41100313538a1ad581df0b13502227e1b6c3
Shaojiasong <lesonky@163.com>
2025-11-13 22:11:34 +0800

    fix(windows): hide console and add missing favicon

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 use chrono::{DateTime, Local};
 use image::{DynamicImage, GenericImageView, ImageBuffer, RgbImage, RgbaImage};
 use libheif_rs::{ColorSpace, HeifContext, RgbChroma};
